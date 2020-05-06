2ddf274875494f44a3e05cebae5855ccf03cbf10
justaweb.dev <3829508-justaweb.dev@users.noreply.gitlab.com>
2020-05-06 23:34:43 +0200

    added spinner

--- src/plantUML.ts
+++ src/plantUML.ts
@@ -1,8 +1,6 @@
 import { LitElement, html, property, customElement } from "lit-element";
 import plantumlEncoder from "plantuml-encoder";
-import {
-  TransitionMap
-} from "./tm/types";
+import { TransitionMap } from "./tm/types";
 
 const head = `
 skinparam state {
@@ -21,8 +19,9 @@ Q1 --> [*]
 
 export const display = (fns: TransitionMap, currentState) => {
   const body = [...fns.entries()]
-    .map(([{ currentQ, inputSymbol }, { nextQ, writeSymbol, direction }]) =>
-      `Q${currentQ} --> Q${nextQ}: ${inputSymbol} / ${writeSymbol},${direction}`
+    .map(
+      ([{ currentQ, inputSymbol }, { nextQ, writeSymbol, direction }]) =>
+        `Q${currentQ} --> Q${nextQ}: ${inputSymbol} / ${writeSymbol},${direction}`
     )
     .join("\n");
   const encoded = plantumlEncoder.encode(
@@ -41,6 +40,20 @@ export class UIDiagram extends LitElement {
   @property()
   currentState: Number;
   render() {
-    return html `<img src=${display(this.transitions, this.currentState)}>`;
+    const div = document.createElement("div");
+    div.className = "loader";
+    div.innerText = "Loading...";
+
+    return html` <link
+        rel="stylesheet"
+        href="./loader.css"
+      />${this.shadowRoot.appendChild(div)}
+
+      <img
+        @load=${() => {
+          div.style.opacity = "0";
+        }}
+        src=${display(this.transitions, this.currentState)}
+      />`;
   }
 }
